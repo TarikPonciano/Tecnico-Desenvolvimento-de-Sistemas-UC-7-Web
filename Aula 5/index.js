56212a3a00a0546cef6b1a80c3d9266c0f2f304c
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-07 20:52:01 -0300

    Fim da aula

--- Aula 5/index.js
+++ Aula 5/index.js
@@ -1,19 +1,32 @@
 function fazerCalculo(){
     const campoNumero1 = document.getElementById("campoNumero1");
     const campoNumero2 = document.getElementById("campoNumero2");
+    const campoOperacao = document.getElementById("operacaoSelecionada")
     const numero1 = parseFloat(campoNumero1.value);
     const numero2 = parseFloat(campoNumero2.value);
+    const operacao = campoOperacao.value
 
     // Verifique se ambos os números foram inseridos. Se ambos os números estão corretos, exibir resultado do cálculo. Se algum número faltou, exibir "Preencha ambos os números"
-    if (!numero1 || !numero2){
+    if (!numero1 || !numero2 || operacao == "escolha" ){
         alert("Preencha os campos corretamente!")
         return
     }
-    const resultado = numero1 + numero2
-    alert(`Resultado da operação: ${resultado}`)
+    let resultado = 0
+    if (operacao == "soma"){
+        resultado = numero1 + numero2
+    } else if (operacao == "subtracao"){
+        resultado = numero1 - numero2
+    } else if (operacao == "multiplicacao"){
+        resultado = numero1 * numero2
+    } else if (operacao == "divisao"){
+        resultado = numero1 / numero2
+    }
+
+    alert(`Resultado da operação de ${operacao}: ${resultado}`)
 
     // Limpe os campos após exibir a mensagem
 
     campoNumero1.value = ""
     campoNumero2.value = ""
+    campoOperacao.selectedIndex = 0
 }
