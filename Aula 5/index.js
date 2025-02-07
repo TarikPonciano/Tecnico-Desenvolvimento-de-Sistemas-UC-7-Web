f71babebbe407329a4e6ed1a9f4042982400770b
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-07 20:24:19 -0300

    Limpar campos

--- Aula 5/index.js
+++ Aula 5/index.js
@@ -1,7 +1,15 @@
 function fazerCalculo(){
-    var numero1 = parseFloat(document.getElementById("campoNumero1").value)
-    var numero2 = parseFloat(document.getElementById("campoNumero2").value)
+    var numero1 = parseFloat(document.getElementById("campoNumero1").value);
+    var numero2 = parseFloat(document.getElementById("campoNumero2").value);
 
     // Verifique se ambos os números foram inseridos. Se ambos os números estão corretos, exibir resultado do cálculo. Se algum número faltou, exibir "Preencha ambos os números"
-    alert(numero1+numero2)
+    if (!numero1 || !numero2){
+        alert("Preencha os campos corretamente!")
+        return
+    }
+    var resultado = numero1 + numero2
+    alert(`Resultado da operação: ${resultado}`)
+
+    // Limpe os campos após exibir a mensagem
+    
 }
