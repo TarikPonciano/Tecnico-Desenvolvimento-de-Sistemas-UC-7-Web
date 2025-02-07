ec1487fb9f42a1d5fc9c856b5a38c84f18d9c268
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-07 20:41:37 -0300

    Criar lista select

--- Aula 5/index.js
+++ Aula 5/index.js
@@ -1,15 +1,19 @@
 function fazerCalculo(){
-    var numero1 = parseFloat(document.getElementById("campoNumero1").value);
-    var numero2 = parseFloat(document.getElementById("campoNumero2").value);
+    const campoNumero1 = document.getElementById("campoNumero1");
+    const campoNumero2 = document.getElementById("campoNumero2");
+    const numero1 = parseFloat(campoNumero1.value);
+    const numero2 = parseFloat(campoNumero2.value);
 
     // Verifique se ambos os números foram inseridos. Se ambos os números estão corretos, exibir resultado do cálculo. Se algum número faltou, exibir "Preencha ambos os números"
     if (!numero1 || !numero2){
         alert("Preencha os campos corretamente!")
         return
     }
-    var resultado = numero1 + numero2
+    const resultado = numero1 + numero2
     alert(`Resultado da operação: ${resultado}`)
 
     // Limpe os campos após exibir a mensagem
-    
+
+    campoNumero1.value = ""
+    campoNumero2.value = ""
 }
