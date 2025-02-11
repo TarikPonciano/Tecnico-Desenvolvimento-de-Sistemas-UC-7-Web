d0af5ff7bb41725fc43b4944820c88e8cec891ab
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-11 18:11:06 -0300

    Inicio Aula 7

---
+++ Aula 7/index.js
@@ -0,0 +1,7 @@
+function cadastrarTarefa(event){
+    event.preventDefault()
+    const nomeTarefa = document.getElementById("campoNomeTarefa").value
+
+    alert(`Tarefa cadastrada com o nome ${nomeTarefa}`)
+    
+}
