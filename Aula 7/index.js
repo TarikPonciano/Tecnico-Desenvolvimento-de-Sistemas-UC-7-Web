9a997edbe2461e5310235f278af385222c2d2418
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-11 19:29:20 -0300

    radio select

--- Aula 7/index.js
+++ Aula 7/index.js
@@ -1,7 +1,24 @@
 function cadastrarTarefa(event){
     event.preventDefault()
     const nomeTarefa = document.getElementById("campoNomeTarefa").value
+    const descricaoTarefa = document.getElementById("campoDescricaoTarefa").value
+    const autorTarefa = document.getElementById("campoAutorTarefa").value
+    const dataTarefa = document.getElementById("campoDataTarefa").value
+    
+    // Solução 2 - Moderna
+    const situacaoFinal = document.querySelector("input[type='radio']:checked").value
 
-    alert(`Tarefa cadastrada com o nome ${nomeTarefa}`)
+    // Solução 1
+    const listaRadio = document.getElementsByName("selecaoTarefa")
+    let situacao = ""
+
+    for (let radio of listaRadio){
+        if (radio.checked){
+            situacao = radio.value
+        }
+    }
+    
+    alert(`Tarefa cadastrada com sucesso! \n Nome: ${nomeTarefa} \n Descrição: ${descricaoTarefa} \n Autor: ${autorTarefa} \n Data: ${dataTarefa} \n Situação: ${situacaoFinal}`)
+    
     
 }
