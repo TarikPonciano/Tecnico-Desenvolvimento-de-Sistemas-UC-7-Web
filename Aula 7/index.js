813c87c779e2509bdc108bdc4bbeebd28134f326
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-11 20:48:04 -0300

    Aula 7 Final

--- Aula 7/index.js
+++ Aula 7/index.js
@@ -8,17 +8,25 @@ function cadastrarTarefa(event){
     // Solução 2 - Moderna
     const situacaoFinal = document.querySelector(".radio-group input[type='radio']:checked").value
 
-    // Solução 1
-    const listaRadio = document.getElementsByName("selecaoTarefa")
-    let situacao = ""
+    // // Solução 1
+    // const listaRadio = document.getElementsByName("selecaoTarefa")
+    // let situacao = ""
 
-    for (let radio of listaRadio){
-        if (radio.checked){
-            situacao = radio.value
-        }
-    }
+    // for (let radio of listaRadio){
+    //     if (radio.checked){
+    //         situacao = radio.value
+    //     }
+    // }
     
     alert(`Tarefa cadastrada com sucesso! \n Nome: ${nomeTarefa} \n Descrição: ${descricaoTarefa} \n Autor: ${autorTarefa} \n Data: ${dataTarefa} \n Situação: ${situacaoFinal}`)
-    
+
+    // Selecionar onde adicionar os novos elementos
+    const listaTarefa = document.getElementById("listaTarefa")
+    // Criar um novo elemento
+    const novaTarefa = document.createElement("li")
+    // Preecher esse elemento
+    novaTarefa.innerHTML = `Nome: ${nomeTarefa} \n Descrição: ${descricaoTarefa} \n Autor: ${autorTarefa} \n Data: ${dataTarefa} \n Situação: ${situacaoFinal}`
+    // Inserir esse elemento no container
+    listaTarefa.appendChild(novaTarefa)
     
 }
