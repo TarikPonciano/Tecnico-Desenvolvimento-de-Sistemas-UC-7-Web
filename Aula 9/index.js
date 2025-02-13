7220c6923e7192c1dcc02a3d18fb853c3926eea3
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-13 20:42:22 -0300

    Final da Aula

---
+++ Aula 9/index.js
@@ -0,0 +1,29 @@
+function cadastrarFuncionario(event){
+    event.preventDefault()
+    const campoNome = document.getElementById("campoNome") 
+    const campoCpf = document.getElementById("campoCpf") 
+    const campoData = document.getElementById("campoData") 
+    const selecaoCargo = document.getElementById("selecaoCargo") 
+    const selecaoEstado = document.getElementById("selecaoEstados") 
+    const radioEstadoCivil = document.querySelector("input[type='radio']:checked")
+    alert(`
+        Nome: ${campoNome.value} \n
+        CPF: ${campoCpf.value} \n
+        Data: ${campoData.value} \n
+        Cargo: ${selecaoCargo.value} \n
+        Estado: ${selecaoEstado.value} \n
+        Estado Civil: ${radioEstadoCivil.value}`)
+
+    const container = document.querySelector(".container")
+    const novoFuncionario = document.createElement("div")
+    novoFuncionario.innerHTML = `
+        <p>Nome: ${campoNome.value}</p> \n
+        CPF: ${campoCpf.value} \n
+        Data: ${campoData.value} \n
+        Cargo: ${selecaoCargo.value} \n
+        Estado: ${selecaoEstado.value} \n
+        Estado Civil: ${radioEstadoCivil.value}`
+    container.appendChild(novoFuncionario)
+
+    
+}
