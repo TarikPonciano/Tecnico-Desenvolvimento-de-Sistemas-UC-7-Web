921d0f7582799b3b6c524bfbf1f6502d01b587ad
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-18 20:47:01 -0300

    Final da Aula

--- Aula 16/atividade.js
+++ Aula 16/atividade.js
@@ -8,4 +8,41 @@
 
 // Após criar o objeto javascript, armazenar esse objeto em uma lista de produtos
 
+// Antes de exibir os produtos no cardápio, altere o preço de todos para refletir um aumento de 10% de imposto.(O preço deve aumentar 10%)
+
 // Ao final, exibir um alert para cada produto mostrando suas informações na tela.
+
+const produtos = []
+
+for (let i = 0; i < 2; i++){
+    alert(`Cadastro do Produto N°${parseInt(i)+1}`)
+    let codigo = parseInt(i)+1
+    let nome = prompt(`Digite o nome do produto ${parseInt(i)+1}:`)
+    let preco = parseFloat(prompt(`Digite o preço do produto ${parseInt(i)+1}:`))
+
+    const produto = {
+        "codigo": codigo,
+        "nome": nome,
+        "preco": preco
+    }
+
+    produtos.push(produto)
+}
+
+const cardapio = produtos.map( produto =>{
+    const novoPreco = produto.preco * 1.1
+    produto.preco = novoPreco.toFixed(2)
+    return produto
+}
+)
+
+produtos.forEach(produto => {
+alert(`
+Produto ${produto.codigo}
+
+Codigo: ${produto.codigo}
+Nome: ${produto.nome}
+Preço: R$ ${produto.preco}
+    
+    `)
+})
