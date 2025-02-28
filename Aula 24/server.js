7772c83638749c7169c32d2b1b51500dfe44baa2
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-28 19:27:37 -0300

    Ajustes css

--- Aula 24/server.js
+++ Aula 24/server.js
@@ -16,9 +16,14 @@ const port = 3000;
 //Capturar nome da pasta atual
 const __dirName = path.resolve();
 
+//Configuração de middlewares
+
+//Tornar público css e arquivos js
+app.use(express.static(path.join(__dirName, "public")))
+
 //Configuração de rota de comunicação com o servidor
 app.get("/", (req, res) =>{
-    res.send("Hello World");
+    res.sendFile(path.join(__dirName, "home.html"))
 })
 
 //Inicializar o servidor
