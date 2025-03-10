144446321f209d592340710ee8900487522c7517
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-10 19:47:50 -0300

    Update server.js

--- Aula 27/server.js
+++ Aula 27/server.js
@@ -1,4 +1,4 @@
-import express from "express"; 
+import express from "express";
 import path from "path";
 import fs from "fs";
 
@@ -9,21 +9,33 @@ const _dirName = path.resolve();
 app.use(express.static(path.join(_dirName, "public")))
 app.use(express.json())
 
+
 app.get("/", (req, res) => {
     res.status(200).sendFile(path.join(_dirName, "views", "home.html"))
 })
 
-app.get("/views/admin/", (req, res) =>{
+app.get("/views/admin/", (req, res) => {
     res.status(200).sendFile(path.join(_dirName, "views", "admin.html"))
 })
 
-app.get("/api/items/", (req, res) => {
-    
+app.get("/api/items", (req, res) => {
+    try {
+        fs.readFile(path.join(_dirName, "data", "items.json"), "utf8", (err, data) => {
+            if (err) {
+                res.status(500).send("Erro na leitura dos arquivos!")
+            } else {
+                const dados = JSON.parse(data)
+                if (dados.length === 0) {
+                    res.status(500).send("Não há arquivos cadastrados")
+                } else {
+                    res.status(200).json(JSON.parse(data))
+                }
+            }
+        })
+    } catch {
+        res.status(500).send("Erro ao acessar o arquivo!")
+    }
 })
-
-app.post("/api/items/", (req, res) => {
-
-});
 
 
 app.listen(port, () => {
