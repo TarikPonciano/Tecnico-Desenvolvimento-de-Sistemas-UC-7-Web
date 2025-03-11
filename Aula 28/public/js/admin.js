ed4675c21737dcf756d78385343e23c1f6171923
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-11 20:50:17 -0300

    Fim da Aula 28

--- Aula 28/public/js/admin.js
+++ Aula 28/public/js/admin.js
@@ -43,7 +43,6 @@ document.addEventListener("DOMContentLoaded", async () => {
         
     })
 
-
 })
 
 async function verDetalhes(id) {
--- Aula 28/server.js
+++ Aula 28/server.js
@@ -115,38 +115,7 @@ app.delete("/api/items/:id", (req, res) => {
     })
 })
 
-// app.delete("/api/items/:id")
 
-
-
-
-
-// app.post("/api/items", (req, res) => {
-//     const dadosNovoProduto = req.body
-//     try{
-//     fs.readFile(path.join(_dirName, "data", "items.json"), "utf8", (err, data) => {
-//         if (err) {
-//             res.status(500).send("Erro na leitura dos arquivos!")
-//         }
-//         else {
-//             const dadosAntigos = JSON.parse(data)
-//             dadosNovoProduto["id"] = dadosAntigos[dadosAntigos.length - 1].id + 1
-//             dadosAntigos.push(dadosNovoProduto)
-
-//             fs.writeFile(path.join(_dirName, "data", "items.json"), JSON.stringify(dadosAntigos, null, 2), (err) => {
-//                 if (err) {
-//                     res.status(500).send("Erro ao escrever o arquivo!")
-//                 } else {
-//                     res.status(201).send("PRODUTO CADASTRADO COM SUCESSO!")
-//                 }
-//             })
-//         }
-//     })
-//     }
-//     catch{
-//         fs.writeFile(path.join(_dirName,"data", ""))
-//     }
-// })
 
 
 app.listen(port, () => {
