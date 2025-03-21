b55a71fec6f238b32fd4a22e971927469fca5602
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-21 20:45:09 -0300

    Update App.jsx

--- Aula 35/meu-primeiro-react/src/App.jsx
+++ Aula 35/meu-primeiro-react/src/App.jsx
@@ -74,21 +74,24 @@ function App() {
     alert(`Livro ${idLivro} removido!`)
   }
 
-  
+
 
   return (
-  // Declaração do que será renderizado
-  <div className='container' style={{color:'Gray'}}>
-    <h1>Acervo de Livros</h1>
-    <ul>
-      {
-        livros.map((livro) => (
-          <CardLivro key = {livro.id} id={livro.id} titulo={livro.titulo} autor={livro.autor} ano_lancamento={livro.ano_lancamento} removerLivro={removerLivro}/>
-        )
-        )
-      }
-    </ul>
-  </div>
+    // Declaração do que será renderizado
+    <>
+      <div className='container'></div>
+      <div className='container' style={{ color: 'Gray' }}>
+        <h1>Acervo de Livros</h1>
+        <ul>
+          {
+            livros.map((livro) => (
+              <CardLivro key={livro.id} id={livro.id} titulo={livro.titulo} autor={livro.autor} ano_lancamento={livro.ano_lancamento} removerLivro={removerLivro} />
+            )
+            )
+          }
+        </ul>
+      </div>
+    </>
   )
 }
 
