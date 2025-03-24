559fe2a02c1efd17aa70494b4715d794cc2c2474
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-24 20:03:26 -0300

    criacao formulario

--- Aula 36/src/components/FormularioNovoLivro/FormularioNovoLivro.jsx
+++ Aula 36/src/components/FormularioNovoLivro/FormularioNovoLivro.jsx
@@ -9,6 +9,15 @@ function FormularioNovoLivro(){
             <h1>Cadastro de Livros</h1>
             <form>
 
+                <label>Titulo</label>
+                <input name='titulo' type="text"  required/>
+
+                <label>Autor</label>
+                <input name='autor' type="text"  required/>
+
+                <label>Ano de Lançamento</label>
+                <input name='ano_lancamento' type="number"  required/>
+
             </form>
         </div>
     )
