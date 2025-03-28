0051af93ce793bb64369928b810461a58759a815
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-28 18:29:26 -0300

    Aula 39

---
+++ Aula 39/src/components/CardLivro/CardLivro.jsx
@@ -0,0 +1,15 @@
+import "./CardLivro.css"
+//Prepare o CardLivro para receber as informações do livro (desctruct)
+function CardLivro({id, titulo, autor, ano_lancamento, removerLivro}){
+    //Lógica
+    
+    //Renderização
+    //Crie um elemento li e preencha com as informações recebidas
+    return(
+        <li>
+           {id} - {titulo} - {autor} - {ano_lancamento} - <button onClick={() => (removerLivro(id))}> 🗑 Apagar</button>
+        </li>
+    )
+}
+
+export default CardLivro
